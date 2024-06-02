70b5a9bd9e2082c3b53bc1a92e74e7f1c3605192
pha-ran <pha-ran@naver.com>
2024-06-02 16:28:58 +0900

    Modify Profiler

--- Profiler.cpp
+++ Profiler.cpp
@@ -1,2 +1,34 @@
 #include "Profiler.h"
 
+struct ProfileData
+{
+	LONGLONG		_flag;
+	TCHAR			_tag[32];
+	LARGE_INTEGER	_begin;
+	LONGLONG		_total;
+	LONGLONG		_call;
+	LONGLONG		_max[2];
+	LONGLONG		_min[2];
+};
+
+ProfileData profileData[32];
+
+VOID ProfileReset(VOID)
+{
+	;
+}
+
+VOID ProfileSave(VOID)
+{
+	;
+}
+
+VOID ProfileBegin(LPCTSTR tag)
+{
+	;
+}
+
+VOID ProfileEnd(LPCTSTR tag)
+{
+	;
+}
--- Profiler.h
+++ Profiler.h
@@ -1,2 +1,21 @@
 #pragma once
+#include "windows.h"
 
+#define PROFILE
+
+#ifdef PROFILE
+	#define PROFILE_RESET()		ProfileReset()
+	#define PROFILE_SAVE()		ProfileSave()
+	#define	PROFILE_BEGIN(TAG)	ProfileBegin(TAG)
+	#define	PROFILE_END(TAG)	ProfileEnd(TAG)
+#else
+	#define PROFILE_RESET()
+	#define PROFILE_SAVE()
+	#define PROFILE_BEGIN(TAG)
+	#define PROFILE_END(TAG)
+#endif
+
+VOID ProfileReset(VOID);
+VOID ProfileSave(VOID);
+VOID ProfileBegin(LPCTSTR tag);
+VOID ProfileEnd(LPCTSTR tag);
