cfb716f987d3766b29fa6ec61c149bc3e5484c23
pha-ran <pha-ran@naver.com>
2024-06-02 19:58:28 +0900

    Modify Profiler

--- Profiler.cpp
+++ Profiler.cpp
@@ -66,7 +66,7 @@ VOID ProfileBegin(LPCTSTR tag)
 	}
 
 	profileData[profileDataCount]._flag = 1;
-	_tcscpy(profileData[profileDataCount]._tag, tag);
+	_tcscpy_s(profileData[profileDataCount]._tag, MAX_TAG_LENGTH, tag);
 	QueryPerformanceCounter(&(profileData[profileDataCount]._begin));
 	profileData[profileDataCount]._call = 1;
 
@@ -77,12 +77,56 @@ VOID ProfileEnd(LPCTSTR tag)
 {
 	static LARGE_INTEGER frequency;
 	INT index;
+	INT trimIndex;
+	LARGE_INTEGER end;
 
 	if (frequency.QuadPart == 0)
 		QueryPerformanceFrequency(&frequency);
 
 	for (index = 0; index < profileDataCount; ++index)
 	{
-		;
+		if (profileData[index]._flag == 0)
+			continue;
+
+		if (_tcscmp(profileData[index]._tag, tag) != 0)
+			continue;
+
+		QueryPerformanceCounter(&end);
+
+		end.QuadPart -= profileData[index]._begin.QuadPart;
+		end.QuadPart *= 10000000;
+		end.QuadPart /= frequency.QuadPart;
+
+		profileData[index]._total += end.QuadPart;
+
+		for (trimIndex = 0; trimIndex < MAX_TRIM; ++trimIndex)
+		{
+			if (end.QuadPart > profileData[index]._max[trimIndex] ||
+				profileData[index]._max[trimIndex] == 0)
+			{
+				profileData[index]._max[trimIndex] = end.QuadPart;
+				break;
+			}
+
+			if (end.QuadPart < profileData[index]._min[trimIndex] ||
+				profileData[index]._min[trimIndex] == 0)
+			{
+				profileData[index]._min[trimIndex] = end.QuadPart;
+				break;
+			}
+		}
+
+		return;
 	}
+}
+
+int main(void)
+{
+	while (1)
+	{
+		PROFILE_BEGIN(_T("Test"));
+		PROFILE_END(_T("Test"));
+	}
+
+	return 0;
 }
