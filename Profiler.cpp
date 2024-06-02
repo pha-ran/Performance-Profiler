bffb06c04d1a1c9c68bfb4a4f882924956ce5b7f
pha-ran <pha-ran@naver.com>
2024-06-02 17:59:06 +0900

    Modify Profiler

--- Profiler.cpp
+++ Profiler.cpp
@@ -1,21 +1,45 @@
 #include "Profiler.h"
+#include <tchar.h>
+
+#define MAX_TAG_LENGTH		32
+#define MAX_TRIM			2
+#define MAX_PROFILE_DATA	32
 
 struct ProfileData
 {
 	LONGLONG		_flag;
-	TCHAR			_tag[32];
+	TCHAR			_tag[MAX_TAG_LENGTH];
 	LARGE_INTEGER	_begin;
-	LONGLONG		_total;
 	LONGLONG		_call;
-	LONGLONG		_max[2];
-	LONGLONG		_min[2];
+	LONGLONG		_total;
+	LONGLONG		_max[MAX_TRIM];
+	LONGLONG		_min[MAX_TRIM];
 };
 
-ProfileData profileData[32];
+static ProfileData	profileData[MAX_PROFILE_DATA];
+static INT			profileDataCount;
 
 VOID ProfileReset(VOID)
 {
-	;
+	INT index;
+	INT trimIndex;
+
+	for (index = 0; index < MAX_PROFILE_DATA; ++index)
+	{
+		profileData[index]._flag = 0;
+		profileData[index]._tag[0] = NULL;
+		profileData[index]._begin.QuadPart = 0;
+		profileData[index]._call = 0;
+		profileData[index]._total = 0;
+		
+		for (trimIndex = 0; trimIndex < MAX_TRIM; ++trimIndex)
+		{
+			profileData[index]._max[trimIndex] = 0;
+			profileData[index]._min[trimIndex] = 0;
+		}
+	}
+
+	profileDataCount = 0;
 }
 
 VOID ProfileSave(VOID)
@@ -25,10 +49,40 @@ VOID ProfileSave(VOID)
 
 VOID ProfileBegin(LPCTSTR tag)
 {
-	;
+	INT index;
+
+	for (index = 0; index < profileDataCount; ++index)
+	{
+		if (profileData[index]._flag == 0)
+			continue;
+
+		if (_tcscmp(profileData[index]._tag, tag) != 0)
+			continue;
+
+		QueryPerformanceCounter(&(profileData[index]._begin));
+		profileData[index]._call += 1;
+
+		return;
+	}
+
+	profileData[profileDataCount]._flag = 1;
+	_tcscpy(profileData[profileDataCount]._tag, tag);
+	QueryPerformanceCounter(&(profileData[profileDataCount]._begin));
+	profileData[profileDataCount]._call = 1;
+
+	++profileDataCount;
 }
 
 VOID ProfileEnd(LPCTSTR tag)
 {
-	;
+	static LARGE_INTEGER frequency;
+	INT index;
+
+	if (frequency.QuadPart == 0)
+		QueryPerformanceFrequency(&frequency);
+
+	for (index = 0; index < profileDataCount; ++index)
+	{
+		;
+	}
 }
